79d9a69237f4fd5544780c4d3da956f5164d000b
rishadroshanpt <rishadroshan27778@gmail.com>
2024-10-10 09:10:33 +0530

    dql learned

--- task1.py
+++ task1.py
@@ -1,13 +1,41 @@
+# import sqlite3
+# con = sqlite3.connect("dbms/task1.db")
+# try:
+#     con.execute("create table std(roll_no int,name text,age int)")
+# except:
+#     pass
+# l=int(input("enter no of students : "))
+# for i in range(l):
+#     roll=i+1
+#     name=input('Enter name : ')
+#     age=int(input('Enter age : '))
+#     con.execute("insert into std(roll_no,name,age)values(?,?,?)",(roll,name,age))
+#     con.commit()
+# data=con.execute("select name from std")
+# for i in data:
+#     print(i)
+
+
+
+
+
+
+
+
+
 import sqlite3
-con = sqlite3.connect("task1.db")
+con = sqlite3.connect("dbms/mark.db")
 try:
     con.execute("create table std(roll_no int,name text,age int)")
+    con.execute("create table mark(roll_no int,sub text,mark int)")
 except:
     pass
-l=int(input("enter no of students : "))
+l=int(input('Enter no of students : '))
 for i in range(l):
-    roll=i+1
+    roll_no=i+1
     name=input('Enter name : ')
     age=int(input('Enter age : '))
-    con.execute("insert into std(roll_no,name,age)values(?,?,?)",(roll,name,age))
+    con.execute("insert into std(roll_no,name,age)values(?,?,?)",(roll_no,name,age))
     con.commit()
+con.execute("insert into mark(roll_no,sub,mark)values(1,'cs',70),(1,'chem',49),(2,'phy',58),(4,'eng',45)")
+con.commit()
--- work1.py
+++ work1.py
@@ -1,13 +1,36 @@
 import sqlite3
-con = sqlite3.connect("work1.db")
+con = sqlite3.connect("dbms/work1.db")
 try:
     con.execute("create table std(roll_no int,name text,age int)")
 except:
     pass
 # con.execute("insert into std(roll_no,name,age)values(1,'marco',16),(2,'eldhose',15)")
 # con.commit()
-roll=int(input('Enter roll no : '))
-name=input('Enter name : ')
-age=int(input('Enter age : '))
-con.execute("insert into std(roll_no,name,age)values(?,?,?)",(roll,name,age))
-con.commit()
+# roll=int(input('Enter roll no : '))
+# name=input('Enter name : ')
+# age=int(input('Enter age : '))
+# con.execute("insert into std(roll_no,name,age)values(?,?,?)",(roll,name,age))
+# con.commit()
+# roll=int(input('Enter roll no of student that you want to select : '))
+# data=con.execute("select * from std where roll_no=?",(roll,))
+# for i in data:
+#     print(i)
+# name=input('Enter name : ')
+# data=con.execute("update std set name='marco paulo',age=17 where name=?",(name,))
+# con.commit()
+# l=int(input("enter no of students : "))
+# for i in range(l):
+#     roll=i+1
+#     name=input('Enter name : ')
+#     age=int(input('Enter age : '))
+#     con.execute("insert into std(roll_no,name,age)values(?,?,?)",(roll,name,age))
+#     con.commit()
+# no=int(input('Enter no : '))
+# con.execute("delete from std where roll_no=?",(no,))
+# con.commit()
+# data=con.execute("select * from std where name like '%a_'")
+# for i in data:
+#     print(i)
+data=con.execute("select * from std order by name desc")
+for i in data:
+    print(i)

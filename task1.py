880ea80b2171b9dd288a0bb0264a7508327f5734
rishadroshanpt <rishadroshan27778@gmail.com>
2024-10-14 08:55:17 +0530

    joined two tables

--- task1.py
+++ task1.py
@@ -30,12 +30,18 @@ try:
     con.execute("create table mark(roll_no int,sub text,mark int)")
 except:
     pass
-l=int(input('Enter no of students : '))
-for i in range(l):
-    roll_no=i+1
-    name=input('Enter name : ')
-    age=int(input('Enter age : '))
-    con.execute("insert into std(roll_no,name,age)values(?,?,?)",(roll_no,name,age))
-    con.commit()
-con.execute("insert into mark(roll_no,sub,mark)values(1,'cs',70),(1,'chem',49),(2,'phy',58),(4,'eng',45)")
-con.commit()
+# l=int(input('Enter no of students : '))
+# for i in range(l):
+#     roll_no=i+1
+#     name=input('Enter name : ')
+#     age=int(input('Enter age : '))
+#     con.execute("insert into std(roll_no,name,age)values(?,?,?)",(roll_no,name,age))
+#     con.commit()
+# con.execute("insert into mark(roll_no,sub,mark)values(1,'cs',70),(1,'chem',49),(2,'phy',58),(4,'eng',45)")
+# con.commit()
+# data=con.execute("select std.roll_no,std.name,std.age,mark.sub,mark.mark from std cross join mark")
+# for i in data:
+#     print(i)
+data=con.execute("select name,count(age) from std group by name")
+for i in data:
+    print(i)
